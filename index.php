7e60722f16d3c181b6153ae9008e3848b1fbf12e
Awwal Bankole <lawybanx35@gmail.com>
2019-04-06 00:11:02 +0100

    First php commit

--- index.php
+++ index.php
@@ -14,8 +14,31 @@
         <button>SUBMIT</button>
     </form>
     <?php
-        $name = $_GET['person'];
-        echo $name. " is a php beginner."
+    // $name = $_GET['person'];
+    // echo $name . " is a php beginner.";
+    // php functions
+    // echo strlen("How many spaces?");
+    // echo str_word_count("How many words?");
+    // print strrev("LawaL MallaM");
+    /*
+        php operaators
+        + - * / % 
+        ** - Exponential
+    */
+    /*
+        php logical operators
+        "or" or ||
+        "and" or &&
+        "xor" -only one conition must be true
+    */
+    $x = 20;
+    $y = 30;
+    if ($x == $y && 1 == 1) {
+        echo "true";
+    } else {
+        echo "false";
+    }
+
     ?>
 </body>
 
